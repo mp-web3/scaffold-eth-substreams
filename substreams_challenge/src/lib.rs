1e77465b45e8e0b11753099993e00aa7a94cfe79
mp-web3 <mp.web3.t@gmail.com>
2024-09-29 19:57:28 +0200

    graph_out module

--- substreams_challenge/src/lib.rs
+++ substreams_challenge/src/lib.rs
@@ -4,12 +4,14 @@ mod rpc;
 use crate::abi::contract::events::Transfer as TransferEvent;
 use crate::rpc::TokenMeta;
 use pb::contract::v1::{Transfer, Transfers};
-use substreams::store::{StoreAdd, StoreAddInt64, StoreNew}; // TODO! 1. (Checkpoint 3.3) import the correct store type and trait
+use substreams::store::{StoreAdd, StoreAddInt64, StoreNew, StoreGetInt64}; // TODO! 1. (Checkpoint 3.3) import the correct store type and trait
+use substreams::store::StoreGet;
 use substreams::Hex;
 use substreams_entity_change::pb::entity::EntityChanges;
 use substreams_entity_change::tables::Tables as EntityChangesTables;
 use substreams_ethereum::pb::eth::v2 as eth;
 use substreams_ethereum::Event;
+
 
 #[allow(unused_imports)]
 use num_traits::cast::ToPrimitive;
@@ -53,32 +55,28 @@ fn map_apes(blk: eth::Block) ->
 
 }
 
-// #[substreams::handlers::store]
-// fn store_transfer_volume(transfers: Transfers) {
-//     todo!("1. add the correct store as the second function argument");
+#[substreams::handlers::store]
+fn store_transfer_volume(transfers: Transfers, store: StoreAddInt64) {
+    // Iterate over the transfers
+    for transfer in transfers.transfers {
+        store.add(0, transfer.address, 1 )
+    }
+}
 
-//     todo!("2. iterate over the transfers");
+#[substreams::handlers::map]
+fn graph_out(store: StoreGetInt64, transfers: Transfers) -> Result<EntityChanges, substreams::errors::Error> {
+    // Initializing EntityChanges container
+    let mut tables = EntityChangesTables::new();
+    
+    for transfer in transfers.transfers {
+        if let Some(value) = store.get_at(0, &transfer.address) {
+            tables.create_row("TransferVolume", &transfer.address)
+            .set("name", transfer.name)
+            .set("symbol", transfer.symbol)
+            .set("address", transfer.address);
+        }
+    }
 
-//     todo!("3. use the `.add()` method on your store to increment the value by 1");
-// }
-
-// #[substreams::handlers::map]
-// fn graph_out() -> Result<EntityChanges, substreams::errors::Error> {
-//     // Initializing EntityChanges container
-//     let mut tables = EntityChangesTables::new();
-
-//     todo!("1. scroll to top of file to add the imports");
-
-//     todo!("2. pass in the store as the first function argument");
-
-//     todo!("3. pass in the second function argument");
-
-//     todo!("4. iterate over the transfers");
-
-//     todo!("5. get the volume from the store");
-
-//     todo!("6. create EntityChanges");
-
-//     // returning EntityChanges
-//     Ok(tables.to_entity_changes())
-// }
+    // returning EntityChanges
+    Ok(tables.to_entity_changes())
+}
